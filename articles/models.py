bce46687227b5a810f8d8cbc1e1508ea60f8c281
yangu1455 <kunja266@hanmail.net>
2022-11-09 02:05:14 +0900

    Image upload - add field

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+'''
+게시판 기능
+- 제목(150글자 이내)
+- 내용(긴 글)
+- 글 작성시간/수정시간(자동으로 기록, 날짜/시간)
+'''
+
 # Create your models here.
+# 1. 모델 설게 (DB 스키마 설계)
 class Review(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/', blank=True)
